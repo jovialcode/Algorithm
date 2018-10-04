0c8017ac76739e876de11444059961652f8568f8
jovialcode <34617984+jovialcode@users.noreply.github.com>
2018-10-04 21:16:41 +0900

    Create 2188.cpp

---
+++ baekjoon/2188.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <cstring>
+
+using namespace std;
+
+vector<vector<int>> node(601);
+int shed[601], visit[601];
+int dfs(int cur){
+    if(visit[cur]) return 0;
+
+    visit[cur] = 1;
+    for(int next : node[cur]){
+        if(!shed[next] || dfs(shed[next])){
+            shed[next] = cur;
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int main() {
+
+    int N, M; scanf("%d %d",&N, &M);
+    int ans = 0;
+    memset(shed, 0, sizeof(shed));
+
+    for(int i = 1; i <= N; i++){
+        int h; scanf("%d",&h);
+        for(int j = 1; j <= h; j++){
+            int house; scanf("%d", &house);
+            node[i].push_back(house);
+        }
+    }
+
+    //입력 정리
+
+    for(int i = 1; i <= N; i++){
+        memset(visit, 0, sizeof(visit));
+        if(dfs(i)){
+            ans++;
+        }
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
