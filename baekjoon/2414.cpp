01330bcc7ac8e95db51be6c88390277384c34fb0
jovialcode <34617984+jovialcode@users.noreply.github.com>
2018-10-23 12:52:00 +0900

    Update 2414.cpp

--- baekjoon/2414.cpp
+++ baekjoon/2414.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int N, M;
 char board[51][51];
 int h[51][51], v[51][51];
-int aMatch[2501],bMatch[2501], visit[2501];
+int match[2501], visit[2501];
 vector<vector<int>> adj(2501);
 
 bool dfs(int start) {
@@ -15,9 +15,8 @@ bool dfs(int start) {
 	visit[start] = true;
 
 	for (int next : adj[start]) {
-		if (bMatch[next] == -1 || dfs(bMatch[next])) { 
-			bMatch[next] = start;
-			aMatch[start] = next;
+		if (match[next] == -1 || dfs(match[next])) { 
+			match[next] = start;
 			return true; 
 		}
 	}
@@ -35,60 +34,38 @@ int main(){
 		getchar();
 	}// 입력정리
 	
+	
+
 	for (int i = 1; i <= N; i++) {
-		int cnt = 0;
-		if (board[i][1] == '.') cnt += 1;
-
 		for (int j = 1; j <= M; j++) {
-			char cur = board[i][j];
-
-			if (cur == '*') { 
-				h[i][j] = aOrder;
-				cnt = 0;
-			}
-			else {
-				if (cnt > 0) cnt += 1;
-				else { 
-					aOrder += 1;
-					cnt += 1; 
-				}
-			}
+			if (board[i][j] == '.') continue;
+			if (j == 1 || h[i][j - 1] == 0)
+				h[i][j] = aOrder++;
+			else
+				h[i][j] = h[i][j - 1];
 		}
-		if (cnt == 0 && i != N) aOrder += 1;
-	}
-	// 가로
+	}//가로
 
-	for (int i = 1; i <= N; i++) {
-		int cnt = 0;
-		if (board[1][i] == '.') cnt += 1;
-
-		for (int j = 1; j <= M; j++) {
-			char cur = board[j][i];
-
-			if (cur == '*') {
-				v[j][i] = bOrder;
-				cnt = 0;
-			}
-			else {
-				if (cnt > 0) cnt += 1;
-				else {
-					bOrder += 1;
-					cnt += 1;
-				}
-			}
+	for (int j = 1; j <= M; j++) {
+		for (int i = 1; i <= N; i++) {
+			if (board[i][j] == '.') continue;
+			if (i == 1 || v[i - 1][j] == 0)
+				v[i][j] = bOrder++;
+			else
+				v[i][j] = v[i - 1][j];
 		}
-		if (cnt == 0 && i != N) bOrder += 1;
-	}//세로
+	}//
+	
 
 	for (int i = 1; i <= N; i++) {
 		for (int j = 1; j <= M; j++) {
+			if (board[i][j] == '.') continue;
 			int ah = h[i][j], bv = v[i][j];
 			adj[ah].push_back(bv);
 		}
 	}//간선 만들기
 	 
-	memset(aMatch, -1, sizeof(aMatch));
-	memset(bMatch, -1, sizeof(bMatch));
+	memset(match, -1, sizeof(match));
 
 	int sol = 0;
 	for (int i = 1; i <= aOrder; i++) {
