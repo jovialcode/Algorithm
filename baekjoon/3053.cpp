eac7fb29ba2ff9f642af0f4a57d2158dd74112bf
jovialcode <34617984+jovialcode@users.noreply.github.com>
2018-12-16 04:11:04 +0900

    Create 3053.cpp
    3053 -원

---
+++ baekjoon/3053.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+const double PI = 3.14159265358979323846;
+
+int main(){
+	double R; scanf("%lf", &R);
+	printf("%0.6f\n", R*R*PI);
+	printf("%0.6f", R*R*2);
+
+}
