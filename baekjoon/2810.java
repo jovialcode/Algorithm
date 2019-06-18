cd31d7481ca3cabb5d94d87d14ca39e65f1fd6b6
jovialcode <34617984+jovialcode@users.noreply.github.com>
2019-06-19 00:52:29 +0900

    Create 2810.java

---
+++ baekjoon/2810.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class algo_2810 {
+    static int N;
+    static String str;
+
+    public static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+
+        str = new StringTokenizer(br.readLine()).nextToken();
+    }
+
+    public static int sol(){
+        int ans = 0;
+        boolean hasL = false;
+        for(int i = 0; i < str.length(); i++){
+            ans += 1;
+            if(str.charAt(i) == 'L') { i += 1; hasL = true;}
+        }
+        if(hasL) ans+=1;
+        return ans;
+    }
+
+    public static void main(String[] args) throws IOException{
+        init();
+        System.out.print(sol());
+    }
+}
