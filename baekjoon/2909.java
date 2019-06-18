85d0e9fd7abc2f66e6743b6102eed2865a334ada
jovialcode <34617984+jovialcode@users.noreply.github.com>
2019-06-18 18:38:07 +0900

    Create 2909.java
    java study

---
+++ baekjoon/2909.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int C, K;
+    static final int[] POWERS_OF_10 = {1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000, 1000000000};
+    static int powerOfTen(int pow) {
+        return POWERS_OF_10[pow];
+    }
+
+    public static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        C = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+    }
+
+    public static int sol(){
+        int ans = C, num, pot = powerOfTen(K);
+        num = C % pot;
+        if(num >= (pot/2)) ans += pot;
+        ans -= num;
+        return ans;
+    }
+
+    public static int intLength(){
+        int len = 0, temp = C;
+        while(temp > 0){
+            len +=1;
+            temp/= 10;
+        }
+        return len;
+    }
+
+    public static void main(String[] args) throws IOException{
+        init();
+        System.out.println(sol());
+    }
+}
